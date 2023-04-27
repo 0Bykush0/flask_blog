59cd31fe45239d242f31606a2fb949c7a2e5e1f7
Bykush <erdemozbaykus@gmail.com>
2023-04-27 18:42:50 +0300

    adding SQLAlchemy database adding sing in and sign up form adding style.css file

--- app.py
+++ app.py
@@ -1,7 +1,46 @@
 from flask import Flask, render_template
 from article_data import Articles
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = True
+
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.String())
+    created_on = db.Column(db.Date, default=datetime.utcnow(), nullable=False)
+
+    def __init__(self, title, description, created_on):
+        self.title = title
+        self.description = description
+        self.created_on = created_on
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(30))
+    registered_on = db.Column(db.Date, default=datetime.utcnow(), nullable=False)
+
+    def __init__(self, name, email, username, password, registered_on):
+        self.name = name
+        self.email = email
+        self.username = username
+        self.password = password
+        self.registered_on = registered_on
+
+
+with app.app_context():
+    db.create_all()
 
 Articles = Articles()
 
